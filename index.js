71fef86165e27e9ac0097f527102a68e7f30b901
Oliver <oli.b.alonso@gmail.com>
2021-01-04 14:18:14 +0100

    to deploy

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ app.use(cors())
 
 app.use(express.json({ extended: true }))
 
-const PORT = process.env.PORT || 4000
+const port = process.env.PORT || 4000
 
 app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/projects', require('./routes/projects'))
 app.use('/api/tasks', require('./routes/tasks'))
 
-app.listen(PORT, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`El servidor esta en el puerto ${PORT}`)
 })
